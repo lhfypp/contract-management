faf5a0b5a96e2f3142239c5305e61e4975979b8c
萧萧寒风 <lvhaifeng@synjones.net>
2019-01-28 14:59:58 +0800

    合同添加租赁地点时间重叠校验

--- contract-business/src/main/java/com/snxy/contract/business/impl/ContractMajorServiceImpl.java
+++ contract-business/src/main/java/com/snxy/contract/business/impl/ContractMajorServiceImpl.java
@@ -4,10 +4,7 @@ import com.snxy.common.exception.BizException;
 import com.snxy.contract.business.utils.ContractSqlEngine;
 import com.snxy.contract.business.utils.ContractTemplateEngine;
 import com.snxy.contract.dao.mapper.ContractMajorMapper;
-import com.snxy.contract.domain.ContractFieldResult;
-import com.snxy.contract.domain.ContractMetaData;
-import com.snxy.contract.domain.MerchantCompany;
-import com.snxy.contract.domain.RentArea;
+import com.snxy.contract.domain.*;
 import com.snxy.contract.service.*;
 import com.snxy.contract.service.vo.AppContractVo;
 import com.snxy.contract.service.vo.ContractEditVo;
@@ -18,9 +15,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.EscapedErrors;
 
 import javax.annotation.Resource;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -41,7 +39,7 @@ public class ContractMajorServiceImpl implements ContractMajorService {
             "drafter_id"
     );
 
-    private final static List<String> moneyFields= Arrays.asList(
+    private final static List<String> moneyFields = Arrays.asList(
             "total_fee"
 
     );
@@ -73,7 +71,7 @@ public class ContractMajorServiceImpl implements ContractMajorService {
         }
 
         //金额特殊处理
-        dealMoney(valueMap,true);
+        dealMoney(valueMap, true);
 
         if (valueMap.get("id") != null && StringUtils.isNotBlank(valueMap.get("id").toString())) {//合同id
             long contractMajorId = Long.parseLong(valueMap.get("id").toString());
@@ -83,23 +81,22 @@ public class ContractMajorServiceImpl implements ContractMajorService {
         return insertContract(creatorId, contractMetaDatas, valueMap);
     }
 
-    private void dealMoney(Map<String, Object> valueMap,boolean isSave) {
-        if(isSave){
-            moneyFields.parallelStream().forEach(moneyField->{
-                if( valueMap.get(moneyField) !=null){
-                    if(StringUtils.isNotBlank(valueMap.get(moneyField).toString())){
-                        long money=Long.parseLong(valueMap.get(moneyField).toString());
-                        valueMap.put(moneyField,(money*100));
+    private void dealMoney(Map<String, Object> valueMap, boolean isSave) {
+        if (isSave) {
+            moneyFields.parallelStream().forEach(moneyField -> {
+                if (valueMap.get(moneyField) != null) {
+                    if (StringUtils.isNotBlank(valueMap.get(moneyField).toString())) {
+                        long money = Long.parseLong(valueMap.get(moneyField).toString());
+                        valueMap.put(moneyField, (money * 100));
                     }
                 }
             });
-        }
-        else {
-            moneyFields.parallelStream().forEach(moneyField->{
-                if( valueMap.get(moneyField) !=null){
-                    if(StringUtils.isNotBlank(valueMap.get(moneyField).toString())){
-                        long money=Long.parseLong(valueMap.get(moneyField).toString());
-                        valueMap.put(moneyField,(money/100));
+        } else {
+            moneyFields.parallelStream().forEach(moneyField -> {
+                if (valueMap.get(moneyField) != null) {
+                    if (StringUtils.isNotBlank(valueMap.get(moneyField).toString())) {
+                        long money = Long.parseLong(valueMap.get(moneyField).toString());
+                        valueMap.put(moneyField, (money / 100));
                     }
                 }
             });
@@ -119,9 +116,10 @@ public class ContractMajorServiceImpl implements ContractMajorService {
         //合同模版id
         extraMap.put("contract_template_category_id", valueMap.get("contract_template_category_id").toString());
         //合同默认费用处理
-        dealExtraMoney(valueMap,extraMap);
+        dealExtraMoney(valueMap, extraMap);
 
         addTime(valueMap, extraMap, true);
+        checkRentTime(contractMetaDatas, valueMap, extraMap, null);
         String insertSql = ContractSqlEngine.getMajorInsrtSql(contractMetaDatas, valueMap, extraMap);
 //        ContractMajor contractMajor = new ContractMajor();
         Map<String, Object> map = new HashMap<String, Object>();
@@ -140,11 +138,11 @@ public class ContractMajorServiceImpl implements ContractMajorService {
     }
 
     private void dealExtraMoney(Map<String, Object> valueMap, Map<String, String> extraMap) {
-        extraMap.put("received_fee","0");//新增已收为0
+        extraMap.put("received_fee", "0");//新增已收为0
         //欠款默认为总金额
-        if(valueMap.get("total_fee")!=null){
-            if(StringUtils.isNotBlank(valueMap.get("total_fee").toString())) {
-                extraMap.put("residual_fee",valueMap.get("total_fee").toString());
+        if (valueMap.get("total_fee") != null) {
+            if (StringUtils.isNotBlank(valueMap.get("total_fee").toString())) {
+                extraMap.put("residual_fee", valueMap.get("total_fee").toString());
             }
         }
     }
@@ -163,8 +161,9 @@ public class ContractMajorServiceImpl implements ContractMajorService {
     private Long updateContract(long creatorId, long contractMajorId, List<ContractMetaData> contractMetaDatas, Map<String, Object> valueMap) {
         Map<String, String> extraMap = new HashMap<>();
         //合同默认费用处理
-        dealExtraMoney(valueMap,extraMap);
+        dealExtraMoney(valueMap, extraMap);
         addTime(valueMap, extraMap, false);
+        checkRentTime(contractMetaDatas, valueMap, extraMap, contractMajorId);
         String updateSql = ContractSqlEngine.getUpdateSql(contractMetaDatas, valueMap, extraMap, contractMajorId);
         log.debug("updateSql:{}", updateSql);
         //  List<ContractFieldResult> dbCfrs = contractFieldResultService.getByContractMajorId(contractMajorId);
@@ -178,6 +177,88 @@ public class ContractMajorServiceImpl implements ContractMajorService {
             contractFieldResultService.batchSave(curCfrs);
         }
         return contractMajorId;
+    }
+
+    /**
+     * 检验是否存在当前签订时间范围内合同
+     *
+     * @param extraMap
+     * @param contractMajorId 排除自己
+     */
+    private void checkRentTime(List<ContractMetaData> contractMetaDatas, Map<String, Object> valueMap, Map<String, String> extraMap, Long contractMajorId) {
+        boolean hasRentTime = contractMetaDatas.parallelStream().anyMatch(cmd -> cmd.getCode().equals("rent_start_time_year"));
+        if (hasRentTime == false)
+            return;
+        String rentStartTime = extraMap.get("rent_start_time");
+        String rentEndTime = extraMap.get("rent_end_time");
+
+        if (StringUtils.isBlank(rentStartTime) || StringUtils.isBlank(rentEndTime)) {
+            throw new BizException("合同起止时间不能为空");
+        }
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+
+        try {
+            Date st= format.parse(rentStartTime);
+            Date et=format.parse(rentEndTime);
+            if(st.after(et)||st.equals(et)){
+                throw new BizException(String.format("合同签订开始时间%s应该小于截止时间%s",rentStartTime,rentEndTime));
+            }
+        } catch (ParseException e) {
+            throw new BizException("合同签订时间转换出错"+e.getMessage());
+           // e.printStackTrace();
+        }
+
+        Long reantAreaId = valueMap.get("rent_area_id") == null ? null : Long.valueOf(valueMap.get("rent_area_id").toString());
+        if (reantAreaId == null)
+            return;
+        String rent_site_no = valueMap.get("rent_site_no") == null ? "" : valueMap.get("rent_site_no").toString();
+        if (StringUtils.isBlank(rent_site_no))
+            return;
+        //查找时间范围内有重叠的相同区域的合同
+        List<ContractMajor> contractMajors = contractMajorMapper.findOverlap(reantAreaId,rentStartTime,rentEndTime,contractMajorId);
+        //比对区域下租赁地点是否有相同的
+        String[] rentSiteNos = rent_site_no.split(",");
+
+//        boolean exist = contractMajors.parallelStream().anyMatch(contractMajor -> {
+//            String innerRentSizteNo = contractMajor.getRentSiteNo();
+//            if (StringUtils.isBlank(innerRentSizteNo))
+//                return false;
+//            String[] innerRentSizteNos = innerRentSizteNo.split(",");
+//            return Arrays.stream(innerRentSizteNos).anyMatch(irs -> {
+//                return true;
+//            });
+//        });
+
+        Map<String, String> existContractInfo = new HashMap<>();
+        contractMajors.parallelStream().forEach(
+                contractMajor -> {
+                    String innerRentSizteNo = contractMajor.getRentSiteNo();
+                    if (StringUtils.isNotBlank(innerRentSizteNo)) {
+                        String[] innerRentSizteNos = innerRentSizteNo.split(",");
+                        for (String rsn : rentSiteNos) {
+                            boolean exist = Arrays.stream(innerRentSizteNos).anyMatch(irsn -> rsn.equals(irsn));
+                            if (exist) {
+                                existContractInfo.put(contractMajor.getContractNo(), rsn);
+                                break;
+                            }
+                        }
+                    }
+                }
+        );
+        if (existContractInfo.size() > 0) {
+            StringBuilder errSb = new StringBuilder();
+            errSb.append("时间与如下合同租赁地点重叠");
+            existContractInfo.forEach((k, v) -> {
+                errSb.append("[合同号:");
+                errSb.append(k);
+                errSb.append("地点编号:");
+                errSb.append(v);
+                errSb.append("]");
+            });
+
+            throw new BizException(errSb.toString());
+        }
     }
 
     @Override
@@ -210,7 +291,7 @@ public class ContractMajorServiceImpl implements ContractMajorService {
         String selectSql = ContractSqlEngine.getSelectSql(contractMetaDatas, contractMajorId, extraFields);
 
         Map<String, Object> valueMap = contractMajorMapper.selectBySql(selectSql);
-        dealMoney(valueMap,false);
+        dealMoney(valueMap, false);
         List<ContractFieldResult> cfrs = contractFieldResultService.getByContractMajorId(contractMajorId);
         cfrs.parallelStream().forEach(cfr -> {
             ContractMetaData contractMetaData = contractMetaDatas.parallelStream().filter(cmd -> cmd.getId().equals(cfr.getContractFieldId())).findFirst().orElse(null);
@@ -378,7 +459,7 @@ public class ContractMajorServiceImpl implements ContractMajorService {
         String endTime = extraMap.get("rent_end_time");
         String stCode = "rent_start_time_year";
         ContractMetaData stcmd = contractMetaDatas.parallelStream().filter(cmd -> stCode.equals(cmd.getCode())).findFirst().orElse(null);
-        if(stcmd!=null) {
+        if (stcmd != null) {
             String stCategory = stcmd.getCategory() == null ? CATEGORY_DEFAULT : stcmd.getCategory();
             //     String name = stcmd.getName().substring(0, stcmd.getName().length() - 1);
             int stOrder = stcmd.getShowOrder() == null ? Integer.MAX_VALUE : stcmd.getShowOrder().intValue();
@@ -400,9 +481,9 @@ public class ContractMajorServiceImpl implements ContractMajorService {
         }
         //合同签订时间
         String signTimeCode = "sign_time_year";
-        String signTimeVal= extraMap.get("sign_time");
+        String signTimeVal = extraMap.get("sign_time");
         ContractMetaData contractMetaData = contractMetaDatas.parallelStream().filter(cmd -> signTimeCode.equals(cmd.getCode())).findFirst().orElse(null);
-        if(contractMetaData!=null) {
+        if (contractMetaData != null) {
             String category = contractMetaData.getCategory() == null ? CATEGORY_DEFAULT : contractMetaData.getCategory();
             String name = contractMetaData.getName().substring(0, contractMetaData.getName().length() - 1);
             int order = contractMetaData.getShowOrder() == null ? Integer.MAX_VALUE : contractMetaData.getShowOrder().intValue();
--- contract-mapper/src/main/java/com/snxy/contract/dao/mapper/ContractMajorMapper.java
+++ contract-mapper/src/main/java/com/snxy/contract/dao/mapper/ContractMajorMapper.java
@@ -3,6 +3,7 @@ package com.snxy.contract.dao.mapper;
 import com.snxy.contract.domain.ContractMajor;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Map;
 
 public interface ContractMajorMapper {
@@ -21,11 +22,13 @@ public interface ContractMajorMapper {
 
     int insertMajorContract(ContractMajor record, @Param(value = "insertSql") String insertSql);
 
-    int insertByMap(Map<String ,Object> map);
+    int insertByMap(Map<String, Object> map);
 
-    int updateBySql(@Param(value = "updateSql")String updateSql);
+    int updateBySql(@Param(value = "updateSql") String updateSql);
 
     Integer getTemplateIdById(Long contractMajorId);
 
-    Map<String,Object> selectBySql(@Param(value = "selectSql")String selectSql);
+    Map<String, Object> selectBySql(@Param(value = "selectSql") String selectSql);
+
+    List<ContractMajor> findOverlap(@Param("rentAreaId") Long rentAreaId, @Param("rentStartTime") String rentStartTime, @Param("rentEndTime") String rentEndTime, @Param("contractMajorId") Long contractMajorId);
 }
